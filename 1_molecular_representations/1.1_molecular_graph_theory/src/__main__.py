8fc598e672abf546678da5bb49d5349517bb68d5
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-06-25 15:26:53 +0100

    Update __main__.py to match sections in 1_molecular_representations README.md and add function to one-hot encode an edge attribute from a networkx.Graph object

--- 1_molecular_representations/1.1_molecular_graph_theory/src/__main__.py
+++ 1_molecular_representations/1.1_molecular_graph_theory/src/__main__.py
@@ -12,59 +12,63 @@ from utils import matrix_utils
 
 def main():
     """
-    Part 1: Use custom MolecularGraphSmilesParser and UndirectedMolecularGraph classes to parse SMILES string of
-            ciprofloxacin into an undirected molecular graph. Test class method logic by printing returns.
+    1.1.1 Introduction To The Molecular Graph Representation
 
-            In reality there is no need to create your own parser and molecular graph classes as many open source
-            packages available (e.g. `networkx`, `pysmiles`, `rdkit.Chem` etc.).
+    Exercise:   Part 1 - Write custom MolecularGraphSmilesParser and UndirectedMolecularGraph classes to parse SMILES
+                string of ciprofloxacin into an undirected molecular graph. Output node & edge attribute data
+
+                Part 2 - In reality there is no need to create your own parser and molecular graph classes as many open
+                source packages are available (e.g. `networkx`, `pysmiles`, `rdkit.Chem` etc.). Use PySMILES package to
+                generate a NetworkX Graph object from ciprofloxacin SMILES string
     """
+    """ Part 1 """
     # Instantiate SMILES to molecular graph parser
     molecular_graph_smiles_parser: MolecularGraphSmilesParser = MolecularGraphSmilesParser()
-
     ciprofloxacin_smiles: str = 'C1CNCCN1c(c2)c(F)cc3c2N(C4CC4)C=C(C3=O)C(=O)O'
     custom_mol_graph = molecular_graph_smiles_parser.smiles_to_molecular_graph(ciprofloxacin_smiles)
 
-    print(f'Ciprofloxacin Molecular = {custom_mol_graph.__str__()}\n')
-
+    print(f'Ciprofloxacin Custom Molecular Graph = {custom_mol_graph.__str__()}\n')
     print(f'Node 0 Attribute Data = {custom_mol_graph.get_node_data(0)}\n')
     print(f'Edge (0, 1) Attribute Data = {custom_mol_graph.get_edge_data((0, 1))}\n')
-    print(f'Node 0 to Node 8 Path = {custom_mol_graph.find_path(0, 8)}\n')
-    print(f'Node 0 to Node 8 Shortest Path = {custom_mol_graph.find_shortest_path(0, 8)}\n')
 
-    print(f'Ciprofloxacin Molecular Graph Connected Nodes = {custom_mol_graph.connected_components()}\n')
-    print(f'Ciprofloxacin Molecular Graph Cycles = {custom_mol_graph.is_cyclic()}\n')
-
-    """
-    Part 2.1: Use PySMILES to parse SMILES string into NetworkX Graph object and convert to adjacency matrix
-    """
+    """ Part 2 """
     # Generate ciprofloxacin networkx.Graph object
     pysmiles_mol_graph: Graph = read_smiles(ciprofloxacin_smiles)
+    print(f'Ciprofloxacin networkx.Graph from PySMILES = {pysmiles_mol_graph.__str__()}\n')
 
-    # Print ciprofloxacin adjacency matrix
+    """
+    1.1.2 Mathematical Definition of a Graph
+    
+    Exercise:   Part 1 - Adjacency Matrix 
+                    Use two approaches to obtain adjacency matrix:
+                        1) Generate adjacency matrix from PySMILES NetworkX Graph object
+                        2) Use RDKit to parse SMILES string into RDKit Mol object, convert to adjacency matrix and 
+                        obtain NetworkX Graph object
+                    
+                Part 2 - Node Features Matrix
+                    Use PySMILES molecular graph to construct node features matrix that utilises one-hot encoding
+                    
+                Part 3 - Edge Features Matrix
+                    Use PySMILES molecular graph to construct edge features matrix that utilises one-hot encoding
+    """
+
+    """  Part 1 - Adjacency Matrix """
+    # 1) Generate adjacency matrix from PySMILES NetworkX Graph object
     nx_adj_matrix: ndarray = networkx.adjacency_matrix(
         pysmiles_mol_graph
     ).todense()
 
     print(f'{nx_adj_matrix}\n')
 
-    """
-    Part 2.2: Use RDKit to parse SMILES string into RDKit Mol object, convert to adjacency matrix and obtain NetworkX 
-            Graph object
-    """
-    # Generate ciprofloxacin Mol object
+    # 2) Use RDKit to parse SMILES string into RDKit Mol object, convert to adjacency matrix and
+    # obtain NetworkX Graph object
     ciprofloxacin_rdkit_mol = MolFromSmiles(ciprofloxacin_smiles)
-
-    # Generate ciprofloxacin adjacency matrix
     rdkit_adj_matrix: Mol = GetAdjacencyMatrix(ciprofloxacin_rdkit_mol, useBO=True)
-
-    # Convert RDKit adjacency matrix to NetworkX Graph
     rdkit_mol_graph: Graph = networkx.from_numpy_array(rdkit_adj_matrix)
 
-    print(f'{rdkit_mol_graph}\n')
+    print(f'{rdkit_adj_matrix}\n')
 
-    """
-    Part 3: Use PySMILES molecular graph to construct node features matrices
-    """
+    """ Part 2 - Node Features """
     # Generate attribute matrix (tuple containing ndarray and matrix key) for `element` node attributes. Matrix shows
     # how many of each element-element bond types there are in the molecule (e.g. C-C, C-O, C-N etc.)
     pysmiles_element_matrix: Tuple = networkx.attr_matrix(
@@ -73,8 +77,49 @@ def main():
     )
     print(f'{pysmiles_element_matrix}\n')
 
-    # Generate one-hot encoded node attribute matrix for `element` node attributes
+    # Generate one-hot encoded node attribute matrix for `element` node attribute
     one_hot_element_matrix = matrix_utils.node_attribute_matrix(pysmiles_mol_graph, 'element')
+    print(f'{one_hot_element_matrix}\n')
+
+    # Generate one-hot encoded node attribute matrix for `aromatic` node attribute
+    one_hot_aromatic_matrix = matrix_utils.node_attribute_matrix(pysmiles_mol_graph, 'aromatic')
+    print(f'{one_hot_aromatic_matrix}\n')
+
+    # Generate one-hot encoded node attribute matrix for `charge` node attribute
+    one_hot_charge_matrix = matrix_utils.node_attribute_matrix(pysmiles_mol_graph, 'charge')
+    print(f'{one_hot_charge_matrix}\n')
+
+    """ Part 3 - Edge Features Matrix """
+    # Generate one-hot encoded edge attribute matrix for `bond_order` edge attribute
+    one_hot_bond_order_matrix = matrix_utils.edge_attribute_matrix(pysmiles_mol_graph, 'order')
+    print(f'{one_hot_bond_order_matrix}\n')
+
+    """
+    1.1.3 Graph Traversal Algorithms
+
+    Exercise:   Part 1 - Depth-First Search Algorithm
+                    Add method to custom UndirectedMolecularGraph class that utilises a depth-first search (DFS) 
+                    algorithm to traverse the graph. Utilise this DFS method to find a path between two nodes, and to 
+                    detect all connected nodes in the graph.
+                
+                Part 2 - Breadth-First Search Algorithm
+                    Add method to custom UndirectedMolecularGraph class that utilises a breadth-first search (BFS) 
+                    algorithm to traverse the graph. Utilise this BFS method to find the shortest path between two 
+                    nodes.
+                
+                Part 3 - Traverse Graph and Detect Cycles
+                    Add method(s) to detect and return any cycles in the molecular graph.
+    """
+
+    """ Part 1 - Depth-First Search Algorithm """
+    print(f'Node 0 to Node 8 Path = {custom_mol_graph.find_path(0, 8)}\n')
+    print(f'Ciprofloxacin Molecular Graph Connected Nodes = {custom_mol_graph.connected_components()}\n')
+
+    """ Part 2 - Breadth-First Search Algorithm """
+    print(f'Node 0 to Node 8 Shortest Path = {custom_mol_graph.find_shortest_path(0, 8)}\n')
+
+    """ Part 3 - Traverse Graph and Detect Cycles """
+    print(f'Ciprofloxacin Molecular Graph Cycles = {custom_mol_graph.is_cyclic()}\n')
 
     test = 'test'
 
--- 1_molecular_representations/1.1_molecular_graph_theory/src/utils/matrix_utils.py
+++ 1_molecular_representations/1.1_molecular_graph_theory/src/utils/matrix_utils.py
@@ -1,5 +1,7 @@
 import pandas
 import networkx
+import numpy
+from typing import Set, List, Any
 
 
 def node_attribute_matrix(graph: networkx.Graph, node_attribute: str):
@@ -16,18 +18,14 @@ def node_attribute_matrix(graph: networkx.Graph, node_attribute: str):
         Returns
         -------
         pandas.DataFrame
-            A DataFrame where rows correspond to nodes and columns correspond to unique elements.
-            Each cell contains 1 if the node has the corresponding element, otherwise 0 (i.e. will be one-hot encoded).
-
-        Notes
-        -----
-        The DataFrame will have nodes as rows and unique attributes as columns, with a binary representation
-        indicating the absence or presence of each element for each node.
+            A DataFrame where rows correspond to nodes and columns correspond to a unique value of the specified node
+            attribute. Each cell contains a binary value indicating absence (0) or presence (1) of the attribute value
+            for each node (i.e. will be one-hot encoded).
 
         Examples
         --------
         >>> import networkx as nx
-        >>> G = nx.Graph()
+        >>> G = networkx.Graph()
         >>> G.add_node(0, element='C')
         >>> G.add_node(1, element='O')
         >>> G.add_node(2, element='C')
@@ -42,17 +40,72 @@ def node_attribute_matrix(graph: networkx.Graph, node_attribute: str):
         2  1  0  0
         3  0  1  0
         """
-    # Extract all unique elements in the graph
-    unique_elements = set(networkx.get_node_attributes(graph, node_attribute).values())
-    unique_elements = sorted(unique_elements)  # Sort for consistent ordering
+    # Extract all unique `node_attribute` values in the graph
+    unique_attr_values: Set = set(networkx.get_node_attributes(graph, node_attribute).values())
+
+    # Sort for consistent ordering
+    unique_attr_values: List = sorted(unique_attr_values)
 
     # Initialize the matrix as a DataFrame
-    num_nodes = len(graph.nodes)
-    element_matrix = pandas.DataFrame(0, index=range(num_nodes), columns=unique_elements)
+    num_nodes: int = len(graph.nodes)
+    node_attr_matrix: pandas.DataFrame = pandas.DataFrame(0, index=range(num_nodes), columns=unique_attr_values)
 
     # Populate the matrix
     for node, data in graph.nodes(data=True):
-        element = data[node_attribute]
-        element_matrix.at[node, element] = 1
+        attr_value = data[node_attribute]
+        node_attr_matrix.at[node, attr_value] = 1
 
-    return element_matrix
+    return node_attr_matrix
+
+
+def edge_attribute_matrix(graph: networkx.Graph, edge_attribute: str):
+    """
+    Create an edge attribute matrix that one-hot encodes a given edge attribute.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        The input graph.
+    edge_attribute : str
+        The edge attribute to be represented in the matrix.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame where rows correspond to edges (represented as a tuple of nodes) and columns corresponds to a
+        unique value of the specified edge attribute. Each cell contains a binary value indicating absence (0) or
+        presence (1) of the attribute value for each edge (i.e. will be one-hot encoded).
+
+    Examples
+    --------
+    >>> G = networkx.Graph()
+    >>> G.add_edge(0, 1, bond_order=1)
+    >>> G.add_edge(1, 2, bond_order=2)
+    >>> G.add_edge(2, 3, bond_order=1)
+    >>> edge_attribute_matrix(G, 'bond_order')
+            1  2
+    (0, 1)  1  0
+    (1, 2)  0  1
+    (2, 3)  1  0
+    """
+    unique_edge_attr_values: Set = set()
+
+    # Extract all unique `edge_attribute` values in the graph
+    for u_node, v_node, data in graph.edges(data=True):
+        if edge_attribute in data:
+            unique_edge_attr_values.add(data[edge_attribute])
+
+    # Sort for consistent ordering
+    unique_edge_attr_values: List = sorted(unique_edge_attr_values)
+
+    # Initialize the matrix
+    edge_keys: List = list(graph.edges)
+    edge_attr_matrix: pandas.DataFrame = pandas.DataFrame(0, index=edge_keys, columns=unique_edge_attr_values)
+
+    # Populate the matrix
+    for u_node, v_node, data in graph.edges(data=True):
+        if edge_attribute in data:
+            attr_value: Any = data[edge_attribute]
+            edge_attr_matrix.at[(u_node, v_node), attr_value] = 1
+
+    return edge_attr_matrix
