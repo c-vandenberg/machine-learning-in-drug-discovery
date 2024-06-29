1ae430d1746137bc5b1332acb7fa92b0045c15ec
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-06-29 01:19:56 +0100

    Add figure images, add new utility functions and update __main__.py

--- 1_molecular_representations/1.2_molecular_descriptors/src/__main__.py
+++ 1_molecular_representations/1.2_molecular_descriptors/src/__main__.py
@@ -2,6 +2,7 @@
 import os
 import pandas
 from pandas import DataFrame
+from numpy import ndarray
 from rdkit import Chem
 from utils import molecular_descriptors_utils
 from constants.smiles_constants import SmilesConstants
@@ -14,7 +15,7 @@ def main():
         * Data set gives SMILES strings and HOMO-LUMO energy gap (in meV).
     """
     mol_descriptors_data_dir: str = os.path.join(os.path.dirname(__file__), '../data')
-    dataset: DataFrame = pandas.read_csv(os.path.join(mol_descriptors_data_dir, 'Orbital_Energies_input_data.csv'))
+    dataset: DataFrame = pandas.read_csv(os.path.join(mol_descriptors_data_dir, 'orbital_energies_input_data.csv'))
 
     """
     2. Generate Canonical SMILES and Remove Duplicate Canonical SMILES
@@ -29,29 +30,83 @@ def main():
     sanitised_dataset: DataFrame = dataset.drop_duplicates(subset=[SmilesConstants.SMILES])
 
     """
-    3. Calculate RDKit Descriptors for All Canonical SMILES in Data Set
+    3. Molecular Descriptors in the RDKit Library
+       In this section, we will generate the following molecular descriptors via the RDKit library:
+        * Molecular ACCess System keys or MACCS-keys
+        * Avalon fingerprint
+        * Atom-pair fingerprint
+        * Topological-Torsions fingerprint.
+        * Morgan fingerprint (Circular Fingerprint)
+        * RDKit Fingerprint
     """
-    # Calculate RDKit descriptors
-    rdkit_mol_descriptors, desc_names = molecular_descriptors_utils.calculate_rdkit_descriptors(
-        sanitised_dataset[SmilesConstants.SMILES]
-    )
-    rdk_descriptors_dataframe = pandas.DataFrame(rdkit_mol_descriptors)
 
     """
-    4. Calculate Morgan Fingerprints for All Canonical SMILES in Data Set
+    3.1  Calculate MACCS-Keys Fingerprint (166 Public Keys) for All Canonical SMILES in Data Set
+         The MACCS-keys are a set of structural keys encoding for a set predefined substructures/fragments, with each 
+         bit indicating the absence or presence of a particular substructure/fragment.
     """
-    morgan_fingerprints = molecular_descriptors_utils.calculate_morgan_fingerprints(
+    maccs_keys_fingerprints: ndarray = molecular_descriptors_utils.calculate_maccs_keys_fingerprints(
         sanitised_dataset[SmilesConstants.SMILES]
     )
-    morgan_fingerprints_dataframe = pandas.DataFrame(
+
+    """
+    3.2 Calculate Avalon Fingerprints for All Canonical SMILES in Data Set
+        Similar to Daylight fingerprints, Avalon uses a fingerprint generator that enumerates certain paths and feature 
+        classes of the molecular graph.
+    """
+    avalon_fingerprints: ndarray = molecular_descriptors_utils.calculate_avalon_fingerprints(
+        sanitised_dataset[SmilesConstants.SMILES]
+    )
+
+    """
+    3.3 Calculate Atom-Pairs Fingerprints for All Canonical SMILES in Data Set
+        Atom-pairs is a simple type of substructure that is defined in terms of the atomic environments of, and 
+        shortest path separation between all pairs of atoms in the topological representation of a chemical structure.
+
+        The atom-pairs fingerprint is an algorithm for computing atom pairs from this aforementioned representation.
+
+    """
+    atom_pairs_fingerprints: ndarray = molecular_descriptors_utils.calculate_atom_pairs_fingerprints(
+        sanitised_dataset[SmilesConstants.SMILES]
+    )
+
+    """
+    3.4 Calculate Topological-Torsions (TT) Fingerprints for All Canonical SMILES in Data Set
+        The topological-torsion (TT) fingerprint is a molecular descriptor that consists of four consecutively bonded 
+        non-hydrogen atoms along with the number of non-hydrogen branches. The descriptor is basically the topological 
+        analogue of the basic conformational element, the torsion angle.
+
+        The atom-pair and TT fingerprints capture and magnify distinct aspects of molecular topology.
+    """
+    tt_fingerprints: ndarray = molecular_descriptors_utils.calculate_tt_fingerprints(
+        sanitised_dataset[SmilesConstants.SMILES]
+    )
+
+    """
+    3.5 Calculate Morgan Fingerprints for All Canonical SMILES in Data Set
+    """
+    morgan_fingerprints: ndarray = molecular_descriptors_utils.calculate_morgan_fingerprints(
+        sanitised_dataset[SmilesConstants.SMILES]
+    )
+    morgan_fingerprints_dataframe: DataFrame = pandas.DataFrame(
         morgan_fingerprints,
         columns=['Col_{}'.format(i) for i in range(morgan_fingerprints.shape[1])]
     )
 
     """
-    5. Calculate Mordred Descriptors for All Canonical SMILES in Data Set
+    3.6 Calculate RDKit Descriptors for All Canonical SMILES in Data Set
     """
-    mordred_descriptors = molecular_descriptors_utils.calculate_mordred_descriptors(
+    # Calculate RDKit descriptors
+    rdkit_mol_descriptors, desc_names = molecular_descriptors_utils.calculate_rdkit_descriptors(
+        sanitised_dataset[SmilesConstants.SMILES]
+    )
+    rdk_descriptors_dataframe: DataFrame = pandas.DataFrame(rdkit_mol_descriptors)
+
+    """
+    3.7 Calculate Mordred Descriptors for All Canonical SMILES in Data Set
+       Mordred is a Python package that can calculate more than 1800 0D, 1D, 2D and 3D molecular descriptors
+    """
+    mordred_descriptors: DataFrame = molecular_descriptors_utils.calculate_mordred_descriptors(
         sanitised_dataset[SmilesConstants.SMILES]
     )
 
--- 1_molecular_representations/1.2_molecular_descriptors/src/utils/molecular_descriptors_utils.py
+++ 1_molecular_representations/1.2_molecular_descriptors/src/utils/molecular_descriptors_utils.py
@@ -1,74 +1,149 @@
 import logging
 import numpy
-from typing import Iterable, Union, List, Tuple
+from typing import Union, List, Tuple
 from numpy import ndarray
+from pandas import Series
 from pandas import DataFrame
 from rdkit import Chem
+from rdkit.Chem import MACCSkeys
+from rdkit.Avalon import pyAvalonTools
 from rdkit.Chem import Descriptors
 from rdkit.Chem import rdMolDescriptors
-from rdkit.ML.Descriptors import MoleculeDescriptors
+from rdkit.Chem import Mol
+from rdkit.DataStructs.cDataStructs import ExplicitBitVect
+from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator
 from mordred import Calculator, descriptors
 
 logger = logging.getLogger(__name__)
 
 
-def generate_canonical_smiles(smiles_dataset: Iterable) -> List:
+def generate_canonical_smiles(smiles_dataset: Series) -> List:
     # Convert SMILES string into RDKit Mol object, which is a molecular graph representation
-    mols = [Chem.MolFromSmiles(smiles_string) for smiles_string in smiles_dataset]
+    mols: List = [Chem.MolFromSmiles(smiles_string) for smiles_string in smiles_dataset]
 
     # Convert each RDKit Mol object back into SMILES string, but in the canonical form
-    canonical_smiles = [Chem.MolToSmiles(mol) for mol in mols]
+    canonical_smiles: List = [Chem.MolToSmiles(mol) for mol in mols]
+
     return canonical_smiles
 
-def calculate_rdkit_descriptors(smiles_dataset: Iterable) -> Union[List, Tuple]:
+
+def calculate_maccs_keys_fingerprints(smiles_dataset: Series) -> ndarray:
+    maccs_keys: List = []
     # Convert each SMILES string in the dataset into an RDKit Mol object
-    mols = [Chem.MolFromSmiles(smiles_string) for smiles_string in smiles_dataset]
+    for smiles_string in smiles_dataset:
+        mol: Mol = Chem.MolFromSmiles(smiles_string)
 
-    # Descriptors.descList is a list of tuples where each tuple contains a descriptor name and its corresponding
-    # function. Here we create a list of descriptor names by extracting the first element (name) from each tuple, and
-    # initialise a descriptor calculator with the list of descriptor names
-    descriptor_calc = MoleculeDescriptors.MolecularDescriptorCalculator(
-        [rdkit_descriptor[0] for rdkit_descriptor in Descriptors.descList]
-    )
-    descriptor_names = descriptor_calc.GetDescriptorNames()
+        # Calculate MACCS-keys fingerprint for Mol object
+        bit_vect_maccs_keys_fingerprint: ExplicitBitVect = MACCSkeys.GenMACCSKeys(mol)
+        maccs_keys_fingerprint: ndarray = numpy.array(bit_vect_maccs_keys_fingerprint)
+        maccs_keys.append(maccs_keys_fingerprint)
 
-    mol_descriptors = []
-    for mol in mols:
-        try:
-            # Some descriptors require explicit hydrogen atoms for accurate calculation
-            mol = Chem.AddHs(mol)
-
-            # Calculate all 200 RDKit descriptors for each molecule (e.g. MolWt, NumAtoms, NumBonds,
-            # NumRotatableBonds etc.)
-            mol_rdkit_descriptors = descriptor_calc.CalcDescriptors(mol)
-            mol_descriptors.append(mol_rdkit_descriptors)
-        except Exception as e:
-            logger.exception(e)
-    return mol_descriptors, descriptor_names
+    return numpy.array(maccs_keys)
 
 
-def calculate_morgan_fingerprints(smiles_dataset: Iterable) -> ndarray:
-    morgan_fingerprints = []
+def calculate_avalon_fingerprints(smiles_dataset: Series) -> ndarray:
+    avalon_fingerprints: List = []
     for smiles_string in smiles_dataset:
         # Convert SMILES string into an RDKit Mol object
-        mol = Chem.MolFromSmiles(smiles_string)
+        mol: Mol = Chem.MolFromSmiles(smiles_string)
+
+        # Calculate Avalon fingerprint for Mol object
+        bit_vect_avalon_fingerprint: ExplicitBitVect = pyAvalonTools.GetAvalonFP(mol, nBits=512)
+        avalon_fingerprint: ndarray = numpy.array(bit_vect_avalon_fingerprint)
+        avalon_fingerprints.append(avalon_fingerprint)
+
+    return numpy.array(avalon_fingerprints)
+
+
+def calculate_atom_pairs_fingerprints(smiles_dataset: Series) -> ndarray:
+    atom_pairs_fingerprints: List = []
+    for smiles_string in smiles_dataset:
+        # Convert SMILES string into an RDKit Mol object
+        mol: Mol = Chem.MolFromSmiles(smiles_string)
+
+        # Calculate atoms-pairs fingerprint for Mol object
+        bit_vect_atom_pairs_fingerprint: ExplicitBitVect = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(
+            mol,
+            nBits=512
+        )
+        atom_pairs_fingerprint: ndarray = numpy.array(bit_vect_atom_pairs_fingerprint)
+        atom_pairs_fingerprints.append(atom_pairs_fingerprint)
+
+    return numpy.array(atom_pairs_fingerprints)
+
+
+def calculate_tt_fingerprints(smiles_dataset: Series) -> ndarray:
+    tt_fingerprints: List = []
+    for smiles_string in smiles_dataset:
+        # Convert SMILES string into an RDKit Mol object
+        mol: Mol = Chem.MolFromSmiles(smiles_string)
+
+        # Calculate topological-torsions fingerprint for Mol object
+        bit_vect_tt_fingerprint: ExplicitBitVect = rdMolDescriptors.GetHashedTopologicalTorsionFingerprintAsBitVect(
+            mol,
+            nBits=512
+        )
+        tt_fingerprint: ndarray = numpy.array(bit_vect_tt_fingerprint)
+        tt_fingerprints.append(tt_fingerprint)
+
+    return numpy.array(tt_fingerprints)
+
+
+def calculate_morgan_fingerprints(smiles_dataset: Series) -> ndarray:
+    morgan_fingerprints: List = []
+    for smiles_string in smiles_dataset:
+        # Convert SMILES string into an RDKit Mol object
+        mol: Mol = Chem.MolFromSmiles(smiles_string)
 
         # Calculate Morgan Fingerprint for Mol object. Morgan Fingerprint radius is set to 2 (i.e. the Morgan algorithm
         # considers the atom itself and the atoms up to 2 bonds away/within 2 "hops"), and the size of the binary vector
         # is set to 2048 bits
-        fingerprints = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 2, 2048)
-        morgan_fingerprint = numpy.array(fingerprints)
+        bit_vect_morg_fingerprint: ExplicitBitVect = rdMolDescriptors.GetMorganFingerprintAsBitVect(
+            mol,
+            2,
+            2048
+        )
+        morgan_fingerprint: ndarray = numpy.array(bit_vect_morg_fingerprint)
         morgan_fingerprints.append(morgan_fingerprint)
+
     return numpy.array(morgan_fingerprints)
 
 
-def calculate_mordred_descriptors(smiles_dataset: Iterable) -> DataFrame:
+def calculate_rdkit_descriptors(smiles_dataset: Series) -> Union[List, Tuple]:
+    # Convert each SMILES string in the dataset into an RDKit Mol object
+    mols: List = [Chem.MolFromSmiles(smiles_string) for smiles_string in smiles_dataset]
+
+    # Descriptors.descList is a list of tuples where each tuple contains a descriptor name and its corresponding
+    # function. Here we create a list of descriptor names by extracting the first element (name) from each tuple, and
+    # initialise a descriptor calculator with the list of descriptor names
+    descriptor_calc: MolecularDescriptorCalculator = MolecularDescriptorCalculator(
+        [rdkit_descriptor[0] for rdkit_descriptor in Descriptors.descList]
+    )
+    descriptor_names: tuple = descriptor_calc.GetDescriptorNames()
+
+    mol_descriptors: List = []
+    for mol in mols:
+        try:
+            # Some descriptors require explicit hydrogen atoms for accurate calculation
+            mol: Mol = Chem.AddHs(mol)
+
+            # Calculate all 200 RDKit descriptors for each molecule (e.g. MolWt, NumAtoms, NumBonds,
+            # NumRotatableBonds etc.)
+            mol_rdkit_descriptors: tuple = descriptor_calc.CalcDescriptors(mol)
+            mol_descriptors.append(mol_rdkit_descriptors)
+        except Exception as e:
+            logger.exception(e)
+
+    return mol_descriptors, descriptor_names
+
+
+def calculate_mordred_descriptors(smiles_dataset: Series) -> DataFrame:
     """
     Mordred is a Python package that can calculate more than 1800 0D, 1D, 2D and 3D molecular descriptors
 
     """
-    mordred_calculator = Calculator(descriptors, ignore_3D=False)
-    mols = [Chem.MolFromSmiles(smiles_strings) for smiles_strings in smiles_dataset]
+    mordred_calculator: Calculator = Calculator(descriptors, ignore_3D=False)
+    mols: List = [Chem.MolFromSmiles(smiles_strings) for smiles_strings in smiles_dataset]
 
     # Calculate all >1800 Mordred descriptors for each Mol in a MordredDataFrame
     return mordred_calculator.pandas(mols)
